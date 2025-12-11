fe8f7bc6cb4e0713ecc70186e89dd64a36554999
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-11 13:53:37 -0500

    Use split, reverse, and join to reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArray = str.split("");
+    return strArray.reverse().join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
