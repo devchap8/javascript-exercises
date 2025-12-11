f17ada8a529d7c81f7c43bee2f16964fe0a2ba59
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-11 14:28:13 -0500

    Write function to add all numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    if (startNum > endNum) {
+        [startNum, endNum] = [endNum, startNum];
+    }
+
+    if (!(Number.isInteger(startNum) && Number.isInteger(endNum))
+        || startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    for (let i=startNum; i<=endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
