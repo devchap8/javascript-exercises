78ec37f2499e0fcaefae7887e1090863d746825d
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-11 14:05:34 -0500

    Use filter to remove items from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removals) {
+    for (removal of removals) {
+        let filteredArr = arr.filter(item => item !== removal)
+        arr = filteredArr;
+    }
+    return arr;
 };
 
 // Do not edit below this line
