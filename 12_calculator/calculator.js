21fb5db93fe47312c16ae0e6d834da7cc75aacc1
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-20 17:27:00 -0500

    Finish calculator functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(((a, b) => a + b), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((a, b) => a * b), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i
+  }
+  return factorial;
 };
+
+console.log(sum([5, 5, 5, 5, 5]));
 
 // Do not edit below this line
 module.exports = {
