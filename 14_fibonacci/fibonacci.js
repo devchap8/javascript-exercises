f609175540221c7a872b6b2021ddbeb194239131
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-20 17:55:42 -0500

    Create fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index === 0) return 0;
+    if (index === 1 || index === 2) return 1;
+    if (index < 0) return "OOPS";
+    let prevNum = 1;
+	let currNum = 1;
+	let sum = 0;
+	for (let i = 3; i <= index; i++) {
+		sum = prevNum + currNum;
+		[prevNum, currNum] = [currNum, sum];
+	}
+	return sum;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
