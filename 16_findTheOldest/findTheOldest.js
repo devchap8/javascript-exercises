f079a51bb5f31a75ce18475b8449206f66ed3793
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-20 18:24:30 -0500

    Make find oldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	return arr.reduce(compareOlder)
 };
+
+function compareOlder(oldest, person) {
+    const today = new Date();
+    const thisYear = today.getFullYear();
+	return ((oldest.yearOfDeath ? oldest.yearOfDeath : thisYear) - oldest.yearOfBirth 
+    > (person.yearOfDeath ? person.yearOfDeath : thisYear) - person.yearOfBirth) 
+    ? oldest : person;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
