a7f70e1de072dfd711f33e9569f92eec8ec8bdd2
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-20 17:37:28 -0500

    Finish palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //remove punctuation, spaces, capitals
+    str = str.toLowerCase();
+    deleteList = [" ", ",", "!", "?", "."];
+    for (item of deleteList) {
+        str = str.replaceAll(item, "");
+    }
+	backwardsStr = str.split("").reverse().join("");
+	return (str === backwardsStr);
 };
 
 // Do not edit below this line
